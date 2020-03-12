3b959dccea05fca7e632dc656fd91b95d49b8c38
Frank Madrid <fmadrid@sbccd.cc.ca.us>
2020-03-12 01:13:46 -0700

    Started Driver for Binary Tree Lab

---
+++ src/LabSolutions/binary_tree/AugementedBinaryTreeDriver.java
@@ -0,0 +1,16 @@
+package LabSolutions.binary_tree;
+
+import net.datastructures.LinkedBinaryTree;
+import net.datastructures.Position;
+
+public final class AugementedBinaryTreeDriver {
+
+	
+	private static LinkedBinaryTree<Integer> tree;
+	public static void main(String[] args) {
+		
+		tree = new LinkedBinaryTree<Integer>();
+		
+	}
+
+}
